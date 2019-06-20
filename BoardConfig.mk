c68c02ae62b51bdd2f82e452c4dec4b542ac2580
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-06-20 13:55:14 +0400

    j53gxx: Remove unnecessary vendorsetup.sh
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,16 +3,9 @@ include device/samsung/j5-common/BoardConfigCommon.mk
 
 LOCAL_PATH := device/samsung/j53gxx
 
-# Asserts
-TARGET_OTA_ASSERT_DEVICE := j53gxx,j53g
-
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j53g_eur_defconfig
 
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 2097152000
 BOARD_USERDATAIMAGE_PARTITION_SIZE  := 5079281664
-
-# RIL
-SIM_COUNT := 2
-BOARD_GLOBAL_CFLAGS += -DRIL_FIX_SMS_NOT_SENT_ERR
--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for i in eng userdebug user; do
-	add_lunch_combo lineage_j53gxx-${i}
-	add_lunch_combo omni_j53gxx-${i}
-done
