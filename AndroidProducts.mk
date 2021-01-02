10862891a5590829bde24746dcf274b1d74fa5ae
palmbeach05 <davmcleod87@gmail.com>
2021-01-01 23:02:21 -0500

    Cleanup
    Remove unnecessary bloat.
    Update vendor setup script.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,3 @@
 LOCAL_PATH := device/samsung/j53gxx
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_j53gxx.mk \
-	$(LOCAL_DIR)/pa_j53gxx.mk \
-	$(LOCAL_DIR)/lineage_j53gxx.mk
+	$(LOCAL_DIR)/omni_j53gxx.mk
